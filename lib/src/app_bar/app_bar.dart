b1518ca52f0e0d295683d73bc477f28b57df8c5a
Michael Elian Kevin <michaelkevin47@gmail.com>
2022-11-10 14:09:37 +0700

    feat(barberia_ui): create default app bar factory

--- lib/src/app_bar/app_bar.dart
+++ lib/src/app_bar/app_bar.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 
+import '../button/button_variant.dart';
+import '../button/icon_button.dart';
 import '../colors.dart';
+import '../text/text.dart';
+import '../widget_size.dart';
 
 class BAppBar extends AppBar {
   BAppBar({
@@ -15,4 +19,23 @@ class BAppBar extends AppBar {
           centerTitle: true,
           elevation: 0,
         );
+
+  factory BAppBar.defaultWithBackButton({
+    required String title,
+    required VoidCallback popHandler,
+  }) =>
+      BAppBar(
+        leading: Center(
+          child: BIconButton.circle(
+            onPressed: popHandler,
+            variant: BButtonVariant.bare,
+            size: BWidgetSize.small,
+            child: const Icon(Icons.arrow_back_ios_new),
+          ),
+        ),
+        title: BText.titleSmall(
+          'Masuk',
+          align: TextAlign.center,
+        ),
+      );
 }
