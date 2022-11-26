ed92e580facd2ef7ae3f649cbf2cd02ae8f10ffe
lyrihkaesa <kaesalyrih@gmail.com>
2022-11-26 16:05:48 +0700

    chore(barberia_uii): create BPaddingSize

--- lib/src/constants.dart
+++ lib/src/constants.dart
@@ -1,3 +1,20 @@
 import 'package:flutter/rendering.dart';
 
 const borderRadius = BorderRadius.all(Radius.circular(8));
+
+enum BPaddingSize { mini, small, medium, large }
+
+extension BPaddingSizeValue on BPaddingSize {
+  double get value {
+    switch (this) {
+      case BPaddingSize.mini:
+        return 4;
+      case BPaddingSize.small:
+        return 8;
+      case BPaddingSize.medium:
+        return 16;
+      case BPaddingSize.large:
+        return 20;
+    }
+  }
+}
